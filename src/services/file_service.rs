d8fa9b502d37e1ba14236d56709c2f2453c2f193
blockorol <blockchainkorol@gmail.com>
2025-09-22 22:10:44 +0200

    [RVL-276] bug fix - external storage

--- src/services/file_service.rs
+++ src/services/file_service.rs
@@ -3,12 +3,22 @@ use std::io::{self, Write};
 use std::path::PathBuf;
 
 const STORAGE_DIR: &str = "./storage";
+fn storage_dir() -> PathBuf {
+    match env::var("STORAGE_DIR") {
+        Ok(path) => PathBuf::from(path),
+        Err(_) => {
+            log::error!("env STORAGE_DIR not found, setted ./storage");
+            PathBuf::from("./storage")
+        }
+    }
+}
 
 pub fn save_png(name: &str, data: &[u8]) -> io::Result<String> {
-    fs::create_dir_all(STORAGE_DIR)?; // создаём директорию, если нет
+    let dir = storage_dir();
+    fs::create_dir_all(&dir)?;
 
     let filename = format!("{name}.png");
-    let mut path = PathBuf::from(STORAGE_DIR);
+    let mut path = dir.clone();
     path.push(&filename);
 
     let mut file = File::create(&path)?;
@@ -18,7 +28,7 @@ pub fn save_png(name: &str, data: &[u8]) -> io::Result<String> {
 }
 
 pub fn load_png(name: &str) -> Option<Vec<u8>> {
-    let mut path = PathBuf::from(STORAGE_DIR);
+    let mut path = storage_dir();
     path.push(name);
     std::fs::read(path).ok()
 }
