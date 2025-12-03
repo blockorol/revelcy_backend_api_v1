101ec390cc2c88d2d352558e5d13e1af6b3a62ad
SNEGUR-D <di.snegur@gmail.com>
2025-12-03 16:40:35 +0100

    add requested fixes

--- migrations/0000005_add_claimed_column.sql
+++ migrations/0000005_add_claimed_column.sql
@@ -2,10 +2,7 @@
 ALTER TABLE premarket_holders
 ADD COLUMN claimed BOOLEAN NOT NULL DEFAULT false;
 
-CREATE INDEX idx_holders_claimed ON premarket_holders(claimed) WHERE claimed = false;
-
 -- +goose Down
-DROP INDEX IF EXISTS idx_holders_claimed;
 ALTER TABLE premarket_holders
 DROP COLUMN IF EXISTS claimed;
 
