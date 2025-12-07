4c03708b6349d3c50fb33c7c4b76584688a3d933
blockorol <blockchainkorol@gmail.com>
2025-12-07 23:43:18 +0100

    fix cors

--- src/middleware/cors.rs
+++ src/middleware/cors.rs
@@ -5,23 +5,23 @@ use std::env;
 pub fn cors_middleware() -> Cors {
     let mut cors = Cors::default()
         .allowed_methods(vec!["GET", "POST", "PUT", "DELETE", "OPTIONS"])
-        .allow_any_header() // <<-- ключевая штука
+        .allow_any_header()
 
         // .allowed_headers(vec![
         //     http::header::AUTHORIZATION,
         //     http::header::CONTENT_TYPE,
         // ])
         .max_age(3600);
-        cors = cors.allow_any_origin();
 
     let origins = env::var("CORS_ORIGINS").unwrap_or_default();
     if origins.trim().is_empty() {
         eprintln!("⚠️  CORS_ORIGINS not set — allow_any_origin()");
+        cors = cors.allow_any_origin();
     } else {
         println!("🔐 Allow CORS with settings: {}", origins);
         for origin in origins.split(',').map(|s| s.trim()).filter(|s| !s.is_empty()) {
             println!("🔐 Allow CORS origin: {}", origin);
-            // cors = cors.allowed_origin(origin);
+            cors = cors.allowed_origin(origin);
         }
     }
 
